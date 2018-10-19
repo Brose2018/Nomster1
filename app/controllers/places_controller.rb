14d1b4ce565cd1a31b8a2abed94871fccfc73183
Brose2018 <jiahmbrose@icloud.com>
2018-10-19 15:15:14 +0000

    lesson 23 updating

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -22,16 +22,18 @@ class PlacesController < ApplicationController
     @place = Place.find(params[:id])
   end
 
+  def update
+    @place = Place.find(params[:id])
+    @place.update_attributes(place_params)
+    redirect_to root_path
+  end
+
   private
 
   def place_params
     params.require(:place).permit(:name, :description, :address)
   end
 
-  def update
-    @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
-  end
+
 
 end
