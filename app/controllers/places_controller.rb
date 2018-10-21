8113f6cafe9855ac1f5c5e7acd882165e4cddc34
Brose2018 <jiahmbrose@icloud.com>
2018-10-21 15:52:58 +0000

    lesson 26 working

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -21,9 +21,9 @@ class PlacesController < ApplicationController
 
   def edit
     @place = Place.find(params[:id])
-
     if @place.user != current_user
       return render plain: 'Not Allowed', status: :forbidden
+    end
   end
 
   def update
